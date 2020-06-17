cb3dfef8af0dad76ce485c106f0a90c464fb6b1b
François Pluchino <francois.pluchino@klipper.dev>
2020-06-17 11:16:07 +0200

    Add api user bundle

---
+++ KlipperApiUserBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Bundle\ApiUserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+class KlipperApiUserBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+Klipper Api User Bundle
+=======================
+
+The Api User bundle provides is sets of the user resources for the API.
+
+Resources
+---------
+
+- [Documentation](https://doc.klipper.dev/bundles/api-user-bundle)
+- [Report issues](https://github.com/klipperdev/klipper/issues)
+  and [send Pull Requests](https://github.com/klipperdev/klipper/pulls)
+  in the [main Klipper repository](https://github.com/klipperdev/klipper)
