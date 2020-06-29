d1ec6100f874d52416aa8d26f7bd73c197453ae9
François Pluchino <francois.pluchino@klipper.dev>
2020-06-29 21:36:47 +0200

    Add api routes to manage info of organization users

--- Controller/OrganizationUserController.php
+++ Controller/OrganizationUserController.php
@@ -12,11 +12,20 @@
 namespace Klipper\Bundle\ApiUserBundle\Controller;
 
 use Klipper\Bundle\ApiBundle\Action\Create;
+use Klipper\Bundle\ApiBundle\Action\Update;
 use Klipper\Bundle\ApiBundle\Controller\Action\Listener\FormPostSubmitListenerInterface;
 use Klipper\Bundle\ApiBundle\Controller\ControllerHelper;
+use Klipper\Bundle\ApiBundle\Exception\InvalidArgumentException;
 use Klipper\Bundle\ApiUserBundle\Form\Type\CreateOrganizationUserType;
+use Klipper\Component\Content\ContentManagerInterface;
+use Klipper\Component\Metadata\MetadataManagerInterface;
 use Klipper\Component\Security\Model\OrganizationUserInterface;
+use Klipper\Component\Security\Model\UserInterface;
 use Klipper\Component\SecurityOauth\Scope\ScopeVote;
+use Klipper\Component\User\Model\ProfileInterface;
+use Klipper\Component\User\Model\Traits\ProfileableInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\Form\Event\PostSubmitEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,6 +40,8 @@ class OrganizationUserController
      * Create a user for the organization.
      *
      * @Route("/organization_users/create", methods={"POST"})
+     *
+     * @Security("is_granted('perm:create', 'App\\Entity\\OrganizationUser')")
      */
     public function create(
         ControllerHelper $helper,
@@ -58,5 +69,116 @@ class OrganizationUserController
                 }
             }, FormPostSubmitListenerInterface::class)
         );
+    }
+
+    /**
+     * Update a user for the organization.
+     *
+     * @Entity(
+     *     "id",
+     *     class="App:OrganizationUser",
+     *     expr="repository.findOrganizationUserById(id)"
+     * )
+     *
+     * @Route("/organization_users/{id}/user", methods={"PATCH"})
+     *
+     * @Security("is_granted('perm:update', id)")
+     */
+    public function updateUser(
+        ControllerHelper $helper,
+        MetadataManagerInterface $metadataManager,
+        OrganizationUserInterface $id
+    ): Response {
+        if (class_exists(ScopeVote::class)) {
+            $helper->denyAccessUnlessGranted(new ScopeVote('meta/organization_user'));
+        }
+
+        $meta = $metadataManager->get(UserInterface::class);
+
+        if (null === $formType = $meta->getFormType()) {
+            throw new InvalidArgumentException(sprintf(
+                'The metadata form type of the "%s" class is required to edit the user info of organization user',
+                $meta->getClass()
+            ));
+        }
+
+        return $helper->update(Update::build(
+            $formType,
+            $id->getUser()
+        ));
+    }
+
+    /**
+     * Update a user profile for the organization.
+     *
+     * @Entity(
+     *     "id",
+     *     class="App:OrganizationUser",
+     *     expr="repository.findOrganizationUserById(id)"
+     * )
+     *
+     * @Route("/organization_users/{id}/profile", methods={"PATCH"})
+     *
+     * @Security("is_granted('perm:update', id)")
+     */
+    public function updateProfile(
+        ControllerHelper $helper,
+        MetadataManagerInterface $metadataManager,
+        OrganizationUserInterface $id
+    ): Response {
+        if (class_exists(ScopeVote::class)) {
+            $helper->denyAccessUnlessGranted(new ScopeVote('meta/organization_user'));
+        }
+
+        $user = $id->getUser();
+
+        if (!$user instanceof ProfileableInterface) {
+            throw $helper->createNotFoundException();
+        }
+
+        $meta = $metadataManager->get(ProfileInterface::class);
+
+        if (null === $formType = $meta->getFormType()) {
+            throw new InvalidArgumentException(sprintf(
+                'The metadata form type of the "%s" class is required to edit the user info of organization user profile',
+                $meta->getClass()
+            ));
+        }
+
+        return $helper->update(Update::build(
+            $formType,
+            $user->getProfile()
+        ));
+    }
+
+    /**
+     * Upload a profile image for the organization user.
+     *
+     * @Entity(
+     *     "id",
+     *     class="App:OrganizationUser",
+     *     expr="repository.findOrganizationUserById(id)"
+     * )
+     *
+     * @Route("/organization_users/{id}/profile/upload", methods={"POST"})
+     *
+     * @Security("is_granted('perm:update', id)")
+     */
+    public function uploadImage(
+        ControllerHelper $helper,
+        ContentManagerInterface $contentManager,
+        OrganizationUserInterface $id
+    ): Response {
+        if (class_exists(ScopeVote::class)) {
+            $helper->denyAccessUnlessGranted(new ScopeVote('meta/organization_user'));
+        }
+
+        $user = $id->getUser();
+
+        if (!$user instanceof ProfileableInterface || null === $user->getProfile()) {
+            throw $helper->createNotFoundException();
+        }
+
+        return $contentManager->upload('user_profile_image', $user->getProfile());
     }
 }
