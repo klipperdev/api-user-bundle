7d6742d0af3f578b70f99be7268087803d4b4d00
François Pluchino <francois.pluchino@klipper.dev>
2020-11-13 20:09:01 +0100

    Allow to edit user info from the org user form type

--- Form/Type/OrganizationUserType.php
+++ Form/Type/OrganizationUserType.php
@@ -13,11 +13,15 @@ namespace Klipper\Bundle\ApiUserBundle\Form\Type;
 
 use Klipper\Bundle\ApiBundle\Form\Type\ObjectMetadataType;
 use Klipper\Component\Form\Doctrine\Type\EntityType;
+use Klipper\Component\Metadata\MetadataManagerInterface;
 use Klipper\Component\Security\Model\OrganizationUserInterface;
 use Klipper\Component\Security\Model\UserInterface;
 use Klipper\Component\Security\Organizational\OrganizationalContextInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Exception\InvalidArgumentException;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Valid;
 
 /**
  * @author François Pluchino <francois.pluchino@klipper.dev>
@@ -26,16 +30,28 @@ class OrganizationUserType extends AbstractType
 {
     private OrganizationalContextInterface $orgContext;
 
-    public function __construct(OrganizationalContextInterface $orgContext)
-    {
+    private MetadataManagerInterface $metadataManager;
+
+    public function __construct(
+        OrganizationalContextInterface $orgContext,
+        MetadataManagerInterface $metadataManager
+    ) {
         $this->orgContext = $orgContext;
+        $this->metadataManager = $metadataManager;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        /** @var OrganizationUserInterface $data */
         $data = $builder->getData();
 
-        if (!$data instanceof OrganizationUserInterface || null !== $data->getId()) {
+        if (!$data instanceof OrganizationUserInterface || (!$options['user_editable'] && null !== $data->getId())) {
+            return;
+        }
+
+        if (null !== $data->getId()) {
+            $this->addUserFields($builder);
+
             return;
         }
 
@@ -51,8 +67,30 @@ class OrganizationUserType extends AbstractType
         ]);
     }
 
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'user_editable' => true,
+        ]);
+    }
+
     public function getParent(): string
     {
         return ObjectMetadataType::class;
+    }
+
+    private function addUserFields(FormBuilderInterface $builder): void
+    {
+        $userMeta = $this->metadataManager->get(UserInterface::class);
+
+        if (null === $userMeta->getFormType()) {
+            throw new InvalidArgumentException('The form type must be defined in the metadata of user model');
+        }
+
+        $builder->add('user', $userMeta->getFormType(), array_merge($userMeta->getFormOptions(), [
+            'constraints' => [
+                new Valid(),
+            ],
+        ]));
     }
 }
